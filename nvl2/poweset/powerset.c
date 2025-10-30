3b57a415ccf300d973b91341e1c2acce51588447
StillNono <antonio.c.nono@gmail.com>
2025-10-30 18:48:16 -0300

    sla

--- nvl2/poweset/powerset.c
+++ nvl2/poweset/powerset.c
@@ -8,7 +8,15 @@ int main(int ac, char **av)
 {
     int alvo = atoi(av[1]);
     int *array;
-
-    array = malloc(ac - 2);
+    int i = 0;
+    int tam = ac - 2;
+    array = malloc(tam * sizeof(int));
+    int *aux_array = malloc(tam*sizeof(int));
+    while (i < tam)
+    {
+        array[i] = av[i + 2];
+        i++;
+    }
+    
 
 }
